1882572146664ae3a45b13c02a62c76463a90a5a
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-07 13:40:32 -0300

    feat(FeedbackController): send feedback message via email
    - new library added: nodemailer

--- src/controllers/FeedbackController.ts
+++ src/controllers/FeedbackController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
 import { prisma } from "../prisma";
+import nodemailer from "nodemailer";
+import { env } from "process";
+
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: env.NODEMAILER_USER,
+    pass: env.NODEMAILER_PASS,
+  },
+});
 
 class FeedbackController {
   async store(req: Request, res: Response) {
@@ -11,6 +22,18 @@ class FeedbackController {
         comment,
         screenshot,
       },
+    });
+
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Pedro Mascarenhas <pedro_drosa@hotmail.com>",
+      subject: "Novo Feedback",
+      html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+        `<p>Tipo do Feedback: ${type}</p>`,
+        `<p>Camentário:${comment}</p>`,
+        `</div>`,
+      ].join("\n"),
     });
 
     return res.status(201).json(feedback);
